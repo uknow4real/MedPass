bd89454796023f969621cae147ebfec3b9e4bdca
Sebastian Chmel <sebastian.chmel@stud.fh-campuswien.ac.at>
2021-06-03 14:19:52 +0200

    warnings fixed

--- app/src/Navigation.js
+++ app/src/Navigation.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+//import Form from 'react-bootstrap/Form';
+//import FormControl from 'react-bootstrap/FormControl';
+//import Button from 'react-bootstrap/Button';
 import logo from "./img/logo.png";
 
 const Navigation = () => {
--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,6 +1,5 @@
 import MedPass from "./contracts/MedPass.json";
-import Web3 from "web3";
-
+//import Web3 from "web3";
 
 const options = {
   web3: {
