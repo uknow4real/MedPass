4e9defa6f2708411589c2fe64c7417fa2536318d
Leo Bowen Wang <59091252+leobowenwang@users.noreply.github.com>
2021-09-26 10:43:53 +0200

    added esp32 micropy files

---
+++ esp/boot.py
@@ -0,0 +1,36 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+import network
+import machine
+from machine import Pin
+import dht
+dht_pin = dht.DHT11(Pin(4))
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+import time #sleep
+import urequests #send request
+import ujson #to send json
+import ubinascii #to convert device id
+
+ssid = 'Sweethome'
+password = '990504990504'
+ip = '192.168.1.6'
+port = 5000
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
