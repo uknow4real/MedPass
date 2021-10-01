ec95861b02eaced4ac2510875f81ebe26291b655
Sebastian Chmel <sebastian.chmel@stud.fh-campuswien.ac.at>
2021-10-01 19:22:17 +0200

    web3contract - from address added

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 const { contractName, abi } = require('./contracts/MedPass.json');
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const { projectId, privateKey, contractAddress } = require('./secrets.json');
+const { projectId, address, privateKey, contractAddress } = require('./secrets.json');
 
 const provider = new HDWalletProvider(privateKey, `https://kovan.infura.io/v3/${projectId}`);
 const web3 = new Web3(provider);
@@ -13,7 +13,9 @@ const options = {
   },
   contracts: [{
     contractName: contractName,
-    web3Contract: new web3.eth.Contract(abi, contractAddress)
+    web3Contract: new web3.eth.Contract(abi, contractAddress, {
+      from: address
+    })
   }]
 };
 
