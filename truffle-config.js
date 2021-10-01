4c2e747c46da8375a5027d70cc9392a9e497cd93
Leo Bowen Wang <59091252+leobowenwang@users.noreply.github.com>
2021-10-01 18:56:26 +0200

    updated truffle config from ropsten to kovan

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const { projectId, privateKey } = require('./secrets.json');
+const { projectId, privateKey, address } = require('./secrets.json');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
-    ropsten: {
-      provider: () => new HDWalletProvider(privateKey, `https://ropsten.infura.io/v3/${projectId}`),
+    kovan: {
+      provider: () => new HDWalletProvider(privateKey, `https://kovan.infura.io/v3/${projectId}`),
       network_id: 3,
       gas: 4000000,
       gasPrice: 10000000000,
