8428cf04f8562db38f1fa849d1e5d92b8582d712
Sebastian Chmel <sebastian.chmel@stud.fh-campuswien.ac.at>
2021-11-03 10:25:53 +0100

    ESP32 files updated

--- esp/boot.py
+++ esp/boot.py
@@ -1,36 +1,42 @@
-import network
-import machine
 from machine import Pin
 import dht
-dht_pin = dht.DHT11(Pin(4))
-
 import esp
-esp.osdebug(None)
-
+import esp32
 import gc
+import machine
+import micropython
+import network
+import ntptime 
+import urequests # send request
+import ujson # to send json
+import ubinascii # to convert device id
+import utime # convert to unix
+import time # sleep
+#
+# Functions
+esp.osdebug(None)
 gc.collect()
-
-import time #sleep
-import urequests #send request
-import ujson #to send json
-import ubinascii #to convert device id
-
+#
+# Variables
+dht_pin = dht.DHT11(Pin(4))
+key = ubinascii.hexlify(machine.unique_id()).decode()
+#
+from hashlib import sha256
+h = sha256()
+h.update(key+str(micropython.stack_use())+str(esp.flash_size()))
+#
 ssid = '' #str
 password = '' #str
 dataUrl = '' #str
 authUrl = '' #str
-pwd = '' #int
-
+pwd = ubinascii.hexlify(h.digest()).decode()
+#
+# Network connect & settime
 station = network.WLAN(network.STA_IF)
 station.active(True)
 station.connect(ssid, password)
-
 while station.isconnected() == False:
   pass
-
 print('Connection successful')
 print(station.ifconfig())
-
-import ntptime # get time from NTP
-ntptime.settime()
-import utime # convert to unix
+ntptime.settime() # get time from NTP
